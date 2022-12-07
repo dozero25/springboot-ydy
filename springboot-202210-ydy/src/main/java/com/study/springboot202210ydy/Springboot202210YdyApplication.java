db919f4d652090f7b439a3fa62a0f028f082fa61
dozero25 <ehdud05264@naver.com>
2022-12-07 21:59:40 +0900

    12/7 코리아IT springboot 설치 및 실습

---
+++ springboot-202210-ydy/src/main/java/com/study/springboot202210ydy/Springboot202210YdyApplication.java
@@ -0,0 +1,13 @@
+package com.study.springboot202210ydy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot202210YdyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot202210YdyApplication.class, args);
+	}
+
+}
---
+++ springboot-202210-ydy/src/test/java/com/study/springboot202210ydy/Springboot202210YdyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.springboot202210ydy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot202210YdyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
