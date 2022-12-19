83ea1075710dc7dd29b079c0fefc416ddfb8ce6b
dozero25 <ehdud05264@naver.com>
2022-12-20 01:30:32 +0900

    12/20 java intellij 설정 추가

---
+++ springbootydy/src/main/java/com/study/springbootydy/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.study.springbootydy.repository;
+
+import com.study.springbootydy.web.dto.UserDto;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface UserRepository {
+    public int saveUser(UserDto userDto);
+    public UserDto findUserById(int userId);
+}
